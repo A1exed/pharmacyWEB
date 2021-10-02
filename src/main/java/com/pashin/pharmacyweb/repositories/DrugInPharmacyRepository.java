bcc34a18235293eeed804008e96b5425866853e3
AlexPashin <alexp280898@gmail.com>
2021-10-02 19:48:49 +0400

    add repository annotations

--- src/main/java/com/pashin/pharmacyweb/repositories/DrugInPharmacyRepository.java
+++ src/main/java/com/pashin/pharmacyweb/repositories/DrugInPharmacyRepository.java
@@ -2,6 +2,8 @@ package com.pashin.pharmacyweb.repositories;
 
 import com.pashin.pharmacyweb.entities.DrugInPharmacyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DrugInPharmacyRepository extends JpaRepository<DrugInPharmacyEntity, DrugInPharmacyEntity.DrugInPharmacyKey> {
 }
--- src/main/java/com/pashin/pharmacyweb/repositories/DrugRepository.java
+++ src/main/java/com/pashin/pharmacyweb/repositories/DrugRepository.java
@@ -2,6 +2,8 @@ package com.pashin.pharmacyweb.repositories;
 
 import com.pashin.pharmacyweb.entities.DrugEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DrugRepository extends JpaRepository<DrugEntity, Long> {
 }
