59564ea6bc1c5418c9287a8fc5603e19025a3f65
AlexPashin <alexp280898@gmail.com>
2021-10-07 02:56:08 +0400

    refactor controller

--- src/main/java/com/pashin/pharmacyweb/controller/DBController.java
+++ src/main/java/com/pashin/pharmacyweb/controller/DBController.java
@@ -1,229 +1,188 @@
 package com.pashin.pharmacyweb.controller;
 
-import com.pashin.pharmacyweb.dto.DrugInPharmacyDTO;
-import com.pashin.pharmacyweb.model.*;
-import com.pashin.pharmacyweb.service.db.*;
+import com.pashin.pharmacyweb.dto.*;
+import com.pashin.pharmacyweb.service.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class DBController {
 
     @Autowired
-    private DrugInPharmacyService drugInPharmacyService;
-
-    @Autowired
-    private DrugService drugService;
-
-    @Autowired
-    private EmployeeService employeeService;
-
-    @Autowired
-    private NetworkService networkService;
-
-    @Autowired
-    private PharmacyService pharmacyService;
+    private DBService dbService;
 
     @GetMapping("/getDrugInPharmacyByID")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public DrugInPharmacyDTO getDrugInPharmacyByID(@RequestParam Long drugID, @RequestParam Long pharmacyID) {
-        DrugInPharmacyModel model = drugInPharmacyService.getByID(new DrugInPharmacyModel.DrugInPharmacyKey(pharmacyID, drugID));
-        PharmacyModel pharmacyModel = model.getPharmacyID();
-        NetworkModel networkModel = pharmacyModel.getNetworkID();
-        DrugModel drugModel = model.getDrugID();
-        return new DrugInPharmacyDTO(pharmacyModel.getPharmacyID(), drugModel.getDrugID(), networkModel.getNetworkName(), pharmacyModel.getAddress(), drugModel.getDrugName(), drugModel.getForm(), drugModel.getManufacturerName(), model.getQuantity());
+        return dbService.getDrugInPharmacyByID(drugID, pharmacyID);
     }
 
     @GetMapping("/getDrugInPharmacyList")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public List<DrugInPharmacyDTO> getDrugInPharmacyList() {
-        List<DrugInPharmacyModel> models = drugInPharmacyService.getAll();
-        List<DrugInPharmacyDTO> results = new ArrayList<>();
-        PharmacyModel pharmacyModel;
-        NetworkModel networkModel;
-        DrugModel drugModel;
-        for (DrugInPharmacyModel model : models) {
-            pharmacyModel = model.getPharmacyID();
-            networkModel = pharmacyModel.getNetworkID();
-            drugModel = model.getDrugID();
-            results.add(new DrugInPharmacyDTO(pharmacyModel.getPharmacyID(), drugModel.getDrugID(), networkModel.getNetworkName(), pharmacyModel.getAddress(), drugModel.getDrugName(), drugModel.getForm(), drugModel.getManufacturerName(), model.getQuantity()));
-        }
-        return results;
+        return dbService.getDrugInPharmacyList();
     }
 
     @GetMapping("/addDrugInPharmacy")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public DrugInPharmacyDTO addDrugInPharmacy(@RequestParam Long pharmacyID, @RequestParam Long drugID, @RequestParam Integer quantity) {
-        DrugInPharmacyModel model = drugInPharmacyService.add(new DrugInPharmacyModel(pharmacyService.getByID(pharmacyID), drugService.getByID(drugID), quantity));
-        PharmacyModel pharmacyModel = model.getPharmacyID();
-        NetworkModel networkModel = pharmacyModel.getNetworkID();
-        DrugModel drugModel = model.getDrugID();
-        return new DrugInPharmacyDTO(pharmacyModel.getPharmacyID(), drugModel.getDrugID(), networkModel.getNetworkName(), pharmacyModel.getAddress(), drugModel.getDrugName(), drugModel.getForm(), drugModel.getManufacturerName(), model.getQuantity());
+        return dbService.addDrugInPharmacy(pharmacyID, drugID, quantity);
     }
 
     @PostMapping("/deleteDrugInPharmacyByID")
     @ResponseStatus(HttpStatus.OK)
     public void deleteDrugInPharmacyByID(@RequestParam Long pharmacyID, @RequestParam Long drugID) {
-        drugInPharmacyService.deleteByID(new DrugInPharmacyModel.DrugInPharmacyKey(pharmacyID, drugID));
+        dbService.deleteDrugInPharmacyByID(pharmacyID, drugID);
     }
 
     @GetMapping("/editDrugInPharmacy")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public DrugInPharmacyDTO editDrugInPharmacy(@RequestParam Long pharmacyID, @RequestParam Long drugID, @RequestParam Integer quantity) {
-        DrugInPharmacyModel model = drugInPharmacyService.getByID(new DrugInPharmacyModel.DrugInPharmacyKey(pharmacyID, drugID));
-        model.setQuantity(quantity);
-        model = drugInPharmacyService.editElement(model);
-        PharmacyModel pharmacyModel = model.getPharmacyID();
-        NetworkModel networkModel = pharmacyModel.getNetworkID();
-        DrugModel drugModel = model.getDrugID();
-        return new DrugInPharmacyDTO(pharmacyModel.getPharmacyID(), drugModel.getDrugID(), networkModel.getNetworkName(), pharmacyModel.getAddress(), drugModel.getDrugName(), drugModel.getForm(), drugModel.getManufacturerName(), model.getQuantity());
+        return dbService.editDrugInPharmacy(pharmacyID, drugID, quantity);
     }
-//TODO: Доделать
+
     @GetMapping("/getDrugByID")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public DrugModel getDrugByID(@RequestParam Long drugID) {
-        return drugService.getByID(drugID);
+    public DrugDTO getDrugByID(@RequestParam Long drugID) {
+        return dbService.getDrugByID(drugID);
     }
 
     @GetMapping("/getDrugList")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public List<DrugModel> getDrugList() {
-        return drugService.getAll();
+    public List<DrugDTO> getDrugList() {
+        return dbService.getDrugList();
     }
 
     @GetMapping("/addDrug")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public DrugModel addDrug(@RequestBody DrugModel drug) {
-        return drugService.add(drug);
+    public DrugDTO addDrug(@RequestParam String drugName, @RequestParam String form, @RequestParam String manufacturerName) {
+        return dbService.addDrug(drugName, form, manufacturerName);
     }
 
     @PostMapping("/deleteDrugByID")
     @ResponseStatus(HttpStatus.OK)
     public void deleteDrugByID(@RequestParam Long drugID) {
-        drugService.deleteByID(drugID);
+        dbService.deleteDrugByID(drugID);
     }
 
     @GetMapping("/editDrug")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public DrugModel editDrug(@RequestBody DrugModel drug) {
-        return drugService.editElement(drug);
+    public DrugDTO editDrug(@RequestParam Long drugID, @RequestParam String drugName, @RequestParam String form, @RequestParam String manufacturerName) {
+        return dbService.editDrug(drugID, drugName, form, manufacturerName);
     }
 
     @GetMapping("/getEmployeeByID")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    @Transactional
-    public EmployeeModel getEmployeeByID(@RequestParam Long employeeID) {
-        return employeeService.getByID(employeeID);
+    public EmployeeDTO getEmployeeByID(@RequestParam Long employeeID) {
+        return dbService.getEmployeeByID(employeeID);
     }
 
     @GetMapping("/getEmployeeList")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public List<EmployeeModel> getEmployeeList() {
-        return employeeService.getAll();
+    public List<EmployeeDTO> getEmployeeList() {
+        return dbService.getEmployeeList();
     }
 
     @GetMapping("/addEmployee")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public EmployeeModel addEmployee(@RequestBody EmployeeModel employee) {
-        return employeeService.add(employee);
+    public EmployeeDTO addEmployee(@RequestParam String employeeName, @RequestParam String position, @RequestParam String phoneNumber, @RequestParam Long pharmacyID) {
+        return dbService.addEmployee(employeeName, position, phoneNumber, pharmacyID);
     }
 
     @PostMapping("/deleteEmployeeByID")
     @ResponseStatus(HttpStatus.OK)
     public void deleteEmployeeByID(@RequestParam Long employeeID) {
-        employeeService.deleteByID(employeeID);
+        dbService.deleteEmployeeByID(employeeID);
     }
 
     @GetMapping("/editEmployee")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public EmployeeModel editEmployee(@RequestBody EmployeeModel employee) {
-        return employeeService.editElement(employee);
+    public EmployeeDTO editEmployee(@RequestParam Long employeeID, @RequestParam String employeeName, @RequestParam String position, @RequestParam String phoneNumber, @RequestParam Long pharmacyID) {
+        return dbService.editEmployee(employeeID, employeeName, position, phoneNumber, pharmacyID);
     }
 
     @GetMapping("/getNetworkByID")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public NetworkModel getNetworkByID(@RequestParam Long networkID) {
-        return networkService.getByID(networkID);
+    public NetworkDTO getNetworkByID(@RequestParam Long networkID) {
+        return dbService.getNetworkByID(networkID);
     }
 
     @GetMapping("/getNetworkList")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public List<NetworkModel> getNetworkList() {
-        return networkService.getAll();
+    public List<NetworkDTO> getNetworkList() {
+        return dbService.getNetworkList();
     }
 
     @GetMapping("/addNetwork")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public NetworkModel addNetwork(@RequestBody NetworkModel network) {
-        return networkService.add(network);
+    public NetworkDTO addNetwork(@RequestParam String networkName, @RequestParam String phoneNumber, @RequestParam String owner) {
+        return dbService.addNetwork(networkName, phoneNumber, owner);
     }
 
     @PostMapping("/deleteNetworkByID")
     @ResponseStatus(HttpStatus.OK)
     public void deleteNetworkByID(@RequestParam Long networkID) {
-        networkService.deleteByID(networkID);
+        dbService.deleteNetworkByID(networkID);
     }
 
     @GetMapping("/editNetwork")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public NetworkModel editNetwork(@RequestBody NetworkModel network) {
-        return networkService.editElement(network);
+    public NetworkDTO editNetwork(@RequestParam Long networkID, @RequestParam String networkName, @RequestParam String phoneNumber, @RequestParam String owner) {
+        return dbService.editNetwork(networkID, networkName, phoneNumber, owner);
     }
 
     @GetMapping("/getPharmacyByID")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public PharmacyModel getPharmacyByID(@RequestParam Long pharmacyID) {
-        return pharmacyService.getByID(pharmacyID);
+    public PharmacyDTO getPharmacyByID(@RequestParam Long pharmacyID) {
+        return dbService.getPharmacyByID(pharmacyID);
     }
 
     @GetMapping("/getPharmacyList")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public List<PharmacyModel> getPharmacyList() {
-        return pharmacyService.getAll();
+    public List<PharmacyDTO> getPharmacyList() {
+        return dbService.getPharmacyList();
     }
 
     @GetMapping("/addPharmacy")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public PharmacyModel addPharmacy(@RequestBody PharmacyModel pharmacy) {
-        return pharmacyService.add(pharmacy);
+    public PharmacyDTO addPharmacy(@RequestParam String address, @RequestParam Double rating, @RequestParam String phoneNumber, @RequestParam Long networkID) {
+        return dbService.addPharmacy(address, rating, phoneNumber, networkID);
     }
 
     @PostMapping("/deletePharmacyByID")
     @ResponseStatus(HttpStatus.OK)
     public void deletePharmacyByID(@RequestParam Long pharmacyID) {
-        pharmacyService.deleteByID(pharmacyID);
+        dbService.deletePharmacyByID(pharmacyID);
     }
 
     @GetMapping("/editPharmacy")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public PharmacyModel editPharmacy(@RequestBody PharmacyModel pharmacy) {
-        return pharmacyService.editElement(pharmacy);
+    public PharmacyDTO editPharmacy(@RequestParam Long pharmacyID, @RequestParam String address, @RequestParam Double rating, @RequestParam String phoneNumber, @RequestParam Long networkID) {
+        return dbService.editPharmacy(pharmacyID, address, rating, phoneNumber, networkID);
     }
 
 }
---
+++ src/main/java/com/pashin/pharmacyweb/service/DBService.java
@@ -0,0 +1,221 @@
+package com.pashin.pharmacyweb.service;
+
+import com.pashin.pharmacyweb.dto.*;
+import com.pashin.pharmacyweb.model.*;
+import com.pashin.pharmacyweb.service.db.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private DrugInPharmacyService drugInPharmacyService;
+
+    @Autowired
+    private DrugService drugService;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private NetworkService networkService;
+
+    @Autowired
+    private PharmacyService pharmacyService;
+
+    public DrugInPharmacyDTO getDrugInPharmacyByID(Long drugID, Long pharmacyID) {
+        DrugInPharmacyModel model = drugInPharmacyService.getByID(new DrugInPharmacyModel.DrugInPharmacyKey(pharmacyID, drugID));
+        PharmacyModel pharmacyModel = model.getPharmacyID();
+        NetworkModel networkModel = pharmacyModel.getNetworkID();
+        DrugModel drugModel = model.getDrugID();
+        return new DrugInPharmacyDTO(pharmacyModel.getPharmacyID(), drugModel.getDrugID(), networkModel.getNetworkName(), pharmacyModel.getAddress(), drugModel.getDrugName(), drugModel.getForm(), drugModel.getManufacturerName(), model.getQuantity());
+    }
+
+    public List<DrugInPharmacyDTO> getDrugInPharmacyList() {
+        List<DrugInPharmacyModel> models = drugInPharmacyService.getAll();
+        List<DrugInPharmacyDTO> results = new ArrayList<>();
+        PharmacyModel pharmacyModel;
+        NetworkModel networkModel;
+        DrugModel drugModel;
+        for (DrugInPharmacyModel model : models) {
+            pharmacyModel = model.getPharmacyID();
+            networkModel = pharmacyModel.getNetworkID();
+            drugModel = model.getDrugID();
+            results.add(new DrugInPharmacyDTO(pharmacyModel.getPharmacyID(), drugModel.getDrugID(), networkModel.getNetworkName(), pharmacyModel.getAddress(), drugModel.getDrugName(), drugModel.getForm(), drugModel.getManufacturerName(), model.getQuantity()));
+        }
+        return results;
+    }
+
+    public DrugInPharmacyDTO addDrugInPharmacy(Long pharmacyID, Long drugID, Integer quantity) {
+        DrugInPharmacyModel model = drugInPharmacyService.add(new DrugInPharmacyModel(pharmacyService.getByID(pharmacyID), drugService.getByID(drugID), quantity));
+        PharmacyModel pharmacyModel = model.getPharmacyID();
+        NetworkModel networkModel = pharmacyModel.getNetworkID();
+        DrugModel drugModel = model.getDrugID();
+        return new DrugInPharmacyDTO(pharmacyModel.getPharmacyID(), drugModel.getDrugID(), networkModel.getNetworkName(), pharmacyModel.getAddress(), drugModel.getDrugName(), drugModel.getForm(), drugModel.getManufacturerName(), model.getQuantity());
+    }
+
+    public void deleteDrugInPharmacyByID(Long pharmacyID, Long drugID) {
+        drugInPharmacyService.deleteByID(new DrugInPharmacyModel.DrugInPharmacyKey(pharmacyID, drugID));
+    }
+
+    public DrugInPharmacyDTO editDrugInPharmacy(Long pharmacyID, Long drugID, Integer quantity) {
+        DrugInPharmacyModel model = drugInPharmacyService.getByID(new DrugInPharmacyModel.DrugInPharmacyKey(pharmacyID, drugID));
+        model.setQuantity(quantity);
+        model = drugInPharmacyService.editElement(model);
+        PharmacyModel pharmacyModel = model.getPharmacyID();
+        NetworkModel networkModel = pharmacyModel.getNetworkID();
+        DrugModel drugModel = model.getDrugID();
+        return new DrugInPharmacyDTO(pharmacyModel.getPharmacyID(), drugModel.getDrugID(), networkModel.getNetworkName(), pharmacyModel.getAddress(), drugModel.getDrugName(), drugModel.getForm(), drugModel.getManufacturerName(), model.getQuantity());
+    }
+
+    public DrugDTO getDrugByID(Long drugID) {
+        DrugModel model = drugService.getByID(drugID);
+        return new DrugDTO(model.getDrugID(), model.getDrugName(), model.getForm(), model.getManufacturerName());
+    }
+
+    public List<DrugDTO> getDrugList() {
+        List<DrugModel> models = drugService.getAll();
+        List<DrugDTO> results = new ArrayList<>();
+        for (DrugModel model : models) {
+            results.add(new DrugDTO(model.getDrugID(), model.getDrugName(), model.getForm(), model.getManufacturerName()));
+        }
+        return results;
+    }
+
+    public DrugDTO addDrug(String drugName, String form, String manufacturerName) {
+        DrugModel model = drugService.add(new DrugModel(drugName, form, manufacturerName));
+        return new DrugDTO(model.getDrugID(), model.getDrugName(), model.getForm(), model.getManufacturerName());
+    }
+
+    public void deleteDrugByID(Long drugID) {
+        drugService.deleteByID(drugID);
+    }
+
+    public DrugDTO editDrug(Long drugID, String drugName, String form, String manufacturerName) {
+        DrugModel model = drugService.getByID(drugID);
+        model.setDrugName(drugName);
+        model.setForm(form);
+        model.setManufacturerName(manufacturerName);
+        model = drugService.editElement(model);
+        return new DrugDTO(model.getDrugID(), model.getDrugName(), model.getForm(), model.getManufacturerName());
+    }
+
+    public EmployeeDTO getEmployeeByID(Long employeeID) {
+        EmployeeModel model = employeeService.getByID(employeeID);
+        PharmacyModel pharmacyModel = model.getPharmacyID();
+        NetworkModel networkModel = pharmacyModel.getNetworkID();
+        return new EmployeeDTO(model.getEmployeeID(), model.getEmployeeName(), model.getPosition(), model.getPhoneNumber(), pharmacyModel.getPharmacyID(), networkModel.getNetworkName(), pharmacyModel.getAddress());
+    }
+
+    public List<EmployeeDTO> getEmployeeList() {
+        List<EmployeeModel> models = employeeService.getAll();
+        List<EmployeeDTO> results = new ArrayList<>();
+        PharmacyModel pharmacyModel;
+        NetworkModel networkModel;
+        for (EmployeeModel model : models) {
+            pharmacyModel = model.getPharmacyID();
+            networkModel = pharmacyModel.getNetworkID();
+            results.add(new EmployeeDTO(model.getEmployeeID(), model.getEmployeeName(), model.getPosition(), model.getPhoneNumber(), pharmacyModel.getPharmacyID(), networkModel.getNetworkName(), pharmacyModel.getAddress()));
+        }
+        return results;
+    }
+
+    public EmployeeDTO addEmployee(String employeeName, String position, String phoneNumber, Long pharmacyID) {
+        PharmacyModel pharmacyModel = pharmacyService.getByID(pharmacyID);
+        EmployeeModel model = employeeService.add(new EmployeeModel(employeeName, position, phoneNumber, pharmacyModel));
+        pharmacyModel = model.getPharmacyID();
+        NetworkModel networkModel = pharmacyModel.getNetworkID();
+        return new EmployeeDTO(model.getEmployeeID(), model.getEmployeeName(), model.getPosition(), model.getPhoneNumber(), pharmacyModel.getPharmacyID(), networkModel.getNetworkName(), pharmacyModel.getAddress());
+    }
+
+    public void deleteEmployeeByID(Long employeeID) {
+        employeeService.deleteByID(employeeID);
+    }
+
+    public EmployeeDTO editEmployee(Long employeeID, String employeeName, String position, String phoneNumber, Long pharmacyID) {
+        EmployeeModel model = employeeService.getByID(employeeID);
+        model.setEmployeeName(employeeName);
+        model.setPosition(position);
+        model.setPhoneNumber(phoneNumber);
+        model.setPharmacyID(pharmacyService.getByID(pharmacyID));
+        model = employeeService.editElement(model);
+        PharmacyModel pharmacyModel = model.getPharmacyID();
+        NetworkModel networkModel = pharmacyModel.getNetworkID();
+        return new EmployeeDTO(model.getEmployeeID(), model.getEmployeeName(), model.getPosition(), model.getPhoneNumber(), pharmacyModel.getPharmacyID(), networkModel.getNetworkName(), pharmacyModel.getAddress());
+    }
+
+    public NetworkDTO getNetworkByID(Long networkID) {
+        NetworkModel model = networkService.getByID(networkID);
+        return new NetworkDTO(model.getNetworkID(), model.getNetworkName(), model.getPhoneNumber(), model.getOwner());
+    }
+
+    public List<NetworkDTO> getNetworkList() {
+        List<NetworkModel> models = networkService.getAll();
+        List<NetworkDTO> results = new ArrayList<>();
+        for (NetworkModel model : models) {
+            results.add(new NetworkDTO(model.getNetworkID(), model.getNetworkName(), model.getPhoneNumber(), model.getOwner()));
+        }
+        return results;
+    }
+
+    public NetworkDTO addNetwork(String networkName, String phoneNumber, String owner) {
+        NetworkModel model = networkService.add(new NetworkModel(networkName, phoneNumber, owner));
+        return new NetworkDTO(model.getNetworkID(), model.getNetworkName(), model.getPhoneNumber(), model.getOwner());
+    }
+
+    public void deleteNetworkByID(Long networkID) {
+        networkService.deleteByID(networkID);
+    }
+
+    public NetworkDTO editNetwork(Long networkID, String networkName, String phoneNumber, String owner) {
+        NetworkModel model = networkService.getByID(networkID);
+        model.setNetworkName(networkName);
+        model.setPhoneNumber(phoneNumber);
+        model.setOwner(owner);
+        model = networkService.editElement(model);
+        return new NetworkDTO(model.getNetworkID(), model.getNetworkName(), model.getPhoneNumber(), model.getOwner());
+    }
+
+    public PharmacyDTO getPharmacyByID(Long pharmacyID) {
+        PharmacyModel model = pharmacyService.getByID(pharmacyID);
+        NetworkModel networkModel = model.getNetworkID();
+        return new PharmacyDTO(model.getPharmacyID(), model.getAddress(), model.getRating(), model.getPhoneNumber(), networkModel.getNetworkID(), networkModel.getNetworkName());
+    }
+
+    public List<PharmacyDTO> getPharmacyList() {
+        List<PharmacyModel> models = pharmacyService.getAll();
+        List<PharmacyDTO> results = new ArrayList<>();
+        NetworkModel networkModel;
+        for (PharmacyModel model : models) {
+            networkModel = model.getNetworkID();
+            results.add(new PharmacyDTO(model.getPharmacyID(), model.getAddress(), model.getRating(), model.getPhoneNumber(), networkModel.getNetworkID(), networkModel.getNetworkName()));
+        }
+        return results;
+    }
+
+    public PharmacyDTO addPharmacy(String address, Double rating, String phoneNumber, Long networkID) {
+        NetworkModel networkModel = networkService.getByID(networkID);
+        PharmacyModel model = pharmacyService.add(new PharmacyModel(address, rating, phoneNumber, networkModel));
+        networkModel = model.getNetworkID();
+        return new PharmacyDTO(model.getPharmacyID(), model.getAddress(), model.getRating(), model.getPhoneNumber(), networkModel.getNetworkID(), networkModel.getNetworkName());
+    }
+
+    public void deletePharmacyByID(Long pharmacyID) {
+        pharmacyService.deleteByID(pharmacyID);
+    }
+
+    public PharmacyDTO editPharmacy(Long pharmacyID, String address, Double rating, String phoneNumber, Long networkID) {
+        PharmacyModel model = pharmacyService.getByID(pharmacyID);
+        model.setAddress(address);
+        model.setRating(rating);
+        model.setPhoneNumber(phoneNumber);
+        model.setNetworkID(networkService.getByID(networkID));
+        model = pharmacyService.editElement(model);
+        NetworkModel networkModel = model.getNetworkID();
+        return new PharmacyDTO(model.getPharmacyID(), model.getAddress(), model.getRating(), model.getPhoneNumber(), networkModel.getNetworkID(), networkModel.getNetworkName());
+    }
+}
